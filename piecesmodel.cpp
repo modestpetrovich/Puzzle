beece3d26ac2d91be1523981fcc30cd6d5594836
modestpetrovich <56129609+modestpetrovich@users.noreply.github.com>
2019-12-19 20:49:56 +0300

    Update piecesmodel.cpp

--- piecesmodel.cpp
+++ piecesmodel.cpp
@@ -1,84 +1,168 @@
-#include "pieceslist.h"
+#include "piecesmodel.h"
 
-#include <QDrag>
-#include <QDragEnterEvent>
+#include <QIcon>
 #include <QMimeData>
+#include <QRandomGenerator>
 
-PiecesList::PiecesList(int pieceSize, QWidget *parent)
-    : QListWidget(parent), PieceSize(pieceSize)
+PiecesModel::PiecesModel(int pieceSize, QObject *parent)
+    : QAbstractListModel(parent), m_PieceSize(pieceSize)
 {
-    setDragEnabled(true);
-    setViewMode(QListView::IconMode);
-    setIconSize(QSize(PieceSize, PieceSize));
-    setSpacing(10);
-    setAcceptDrops(true);
-    setDropIndicatorShown(true);
 }
 
-void PiecesList::dragEnterEvent(QDragEnterEvent *event)
+QVariant PiecesModel::data(const QModelIndex &index, int role) const
 {
-    if (event->mimeData()->hasFormat(PiecesList::puzzleMimeType()))
-        event->accept();
+    if (!index.isValid())
+        return QVariant();
+
+    if (role == Qt::DecorationRole)
+        return QIcon(pixmaps.value(index.row()).scaled(m_PieceSize, m_PieceSize,
+                         Qt::KeepAspectRatio, Qt::SmoothTransformation));
+    else if (role == Qt::UserRole)
+        return pixmaps.value(index.row());
+    else if (role == Qt::UserRole + 1)
+        return locations.value(index.row());
+
+    return QVariant();
+}
+
+void PiecesModel::addPiece(const QPixmap &pixmap, const QPoint &location)
+{
+    int row;
+    if (QRandomGenerator::global()->bounded(2) == 1)
+        row = 0;
     else
-        event->ignore();
+        row = pixmaps.size();
+
+    beginInsertRows(QModelIndex(), row, row);
+    pixmaps.insert(row, pixmap);
+    locations.insert(row, location);
+    endInsertRows();
 }
 
-void PiecesList::dragMoveEvent(QDragMoveEvent *event)
+Qt::ItemFlags PiecesModel::flags(const QModelIndex &index) const
 {
-    if (event->mimeData()->hasFormat(PiecesList::puzzleMimeType())) {
-        event->setDropAction(Qt::MoveAction);
-        event->accept();
-    } else {
-        event->ignore();
+    if (index.isValid())
+        return (QAbstractListModel::flags(index)|Qt::ItemIsDragEnabled);
+
+    return Qt::ItemIsDropEnabled;
+}
+
+bool PiecesModel::removeRows(int row, int count, const QModelIndex &parent)
+{
+    if (parent.isValid())
+        return false;
+
+    if (row >= pixmaps.size() || row + count <= 0)
+        return false;
+
+    int beginRow = qMax(0, row);
+    int endRow = qMin(row + count - 1, pixmaps.size() - 1);
+
+    beginRemoveRows(parent, beginRow, endRow);
+
+    while (beginRow <= endRow) {
+        pixmaps.removeAt(beginRow);
+        locations.removeAt(beginRow);
+        ++beginRow;
     }
+
+    endRemoveRows();
+    return true;
 }
 
-void PiecesList::dropEvent(QDropEvent *event)
+QStringList PiecesModel::mimeTypes() const
 {
-    if (event->mimeData()->hasFormat(PiecesList::puzzleMimeType())) {
-        QByteArray pieceData = event->mimeData()->data(PiecesList::puzzleMimeType());
-        QDataStream dataStream(&pieceData, QIODevice::ReadOnly);
+    QStringList types;
+    types << "qweqwe";
+    return types;
+}
+
+QMimeData *PiecesModel::mimeData(const QModelIndexList &indexes) const
+{
+    QMimeData *mimeData = new QMimeData();
+    QByteArray encodedData;
+
+    QDataStream stream(&encodedData, QIODevice::WriteOnly);
+
+    foreach (QModelIndex index, indexes) {
+        if (index.isValid()) {
+            QPixmap pixmap = qvariant_cast<QPixmap>(data(index, Qt::UserRole));
+            QPoint location = data(index, Qt::UserRole+1).toPoint();
+            stream << pixmap << location;
+        }
+    }
+
+    mimeData->setData("qweqwe", encodedData);
+    return mimeData;
+}
+
+bool PiecesModel::dropMimeData(const QMimeData *data, Qt::DropAction action,
+                               int row, int column, const QModelIndex &parent)
+{
+    if (!data->hasFormat("qweqwe"))
+        return false;
+
+    if (action == Qt::IgnoreAction)
+        return true;
+
+    if (column > 0)
+        return false;
+
+    int endRow;
+
+    if (!parent.isValid()) {
+        if (row < 0)
+            endRow = pixmaps.size();
+        else
+            endRow = qMin(row, pixmaps.size());
+    } else {
+        endRow = parent.row();
+    }
+
+    QByteArray encodedData = data->data("qweqwe");
+    QDataStream stream(&encodedData, QIODevice::ReadOnly);
+
+    while (!stream.atEnd()) {
         QPixmap pixmap;
         QPoint location;
-        dataStream >> pixmap >> location;
+        stream >> pixmap >> location;
 
-        addPiece(pixmap, location);
+        beginInsertRows(QModelIndex(), endRow, endRow);
+        pixmaps.insert(endRow, pixmap);
+        locations.insert(endRow, location);
+        endInsertRows();
 
-        event->setDropAction(Qt::MoveAction);
-        event->accept();
-    } else {
-        event->ignore();
+        ++endRow;
     }
+
+    return true;
 }
 
-void PiecesList::addPiece(const QPixmap &pixmap, const QPoint &location)
+int PiecesModel::rowCount(const QModelIndex &parent) const
 {
-    QListWidgetItem *pieceItem = new QListWidgetItem(this);
-    pieceItem->setIcon(QIcon(pixmap));
-    pieceItem->setData(Qt::UserRole, QVariant(pixmap));
-    pieceItem->setData(Qt::UserRole+1, location);
-    pieceItem->setFlags(Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsDragEnabled);
+    if (parent.isValid())
+        return 0;
+    else
+        return pixmaps.size();
 }
 
-void PiecesList::startDrag(Qt::DropActions /*supportedActions*/)
+Qt::DropActions PiecesModel::supportedDropActions() const
 {
-    QListWidgetItem *item = currentItem();
+    return Qt::CopyAction | Qt::MoveAction;
+}
 
-    QByteArray itemData;
-    QDataStream dataStream(&itemData, QIODevice::WriteOnly);
-    QPixmap pixmap = qvariant_cast<QPixmap>(item->data(Qt::UserRole));
-    QPoint location = item->data(Qt::UserRole+1).toPoint();
-
-    dataStream << pixmap << location;
-
-    QMimeData *mimeData = new QMimeData;
-    mimeData->setData(PiecesList::puzzleMimeType(), itemData);
-
-    QDrag *drag = new QDrag(this);
-    drag->setMimeData(mimeData);
-    drag->setHotSpot(QPoint(pixmap.width()/2, pixmap.height()/2));
-    drag->setPixmap(pixmap);
-
-    if (drag->exec(Qt::MoveAction) == Qt::MoveAction)
-        delete takeItem(row(item));
+void PiecesModel::addPieces(const QPixmap& pixmap)
+{
+    if (!pixmaps.isEmpty()) {
+        beginRemoveRows(QModelIndex(), 0, pixmaps.size() - 1);
+        pixmaps.clear();
+        locations.clear();
+        endRemoveRows();
+    }
+    for (int y = 0; y < 5; ++y) {
+        for (int x = 0; x < 5; ++x) {
+            QPixmap pieceImage = pixmap.copy(x*m_PieceSize, y*m_PieceSize, m_PieceSize, m_PieceSize);
+            addPiece(pieceImage, QPoint(x, y));
+        }
+    }
 }
